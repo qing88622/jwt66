9de445b6c35c9f2288e6fe7efba61f8bbeedcdb2
qing <judy88622@gmail.com>
2023-05-17 07:03:11 +0800

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new AuthController instance
+     * 
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['credentialslogin']]);
+    }
+    /**
+     * Get a JWT via given credentials.
+     * 
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+        if (!$token = Auth::guard()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorize'], 401);
+        }
+        return $this->respondWithToken($token);
+    }
+    /**
+     * Get a authenticated User.
+     * 
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(Auth::guard()->user());
+    }
+    /**
+     * Log the user out(Invalidate the Token).
+     * 
+     * @return \Illuminate\Http\JsonResponse
+     */
+    
+    public function logout()
+    {
+        Auth::guard()->logout();
+        return response()->json(['message'=>'Successfully logged out']);
+    }
+    /**
+     * Refresh a Token.
+     * 
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(Auth::guard()->refresh());
+    }
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token'=>$token,
+            'token_type'=>'bearer',
+            'expires_in'=>Auth::guard()->factory()->getTTL()*60
+        ]);
+    }
+}
